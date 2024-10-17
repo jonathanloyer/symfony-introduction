dc070b492fc775ba605fd4be33187eea0524f9e7
jochouca <jonathanloyer1@gmail.com>
2024-10-17 10:22:30 +0200

    twelve

--- src/Controller/AuteurController.php
+++ src/Controller/AuteurController.php
@@ -5,111 +5,93 @@ namespace App\Controller;
 use App\Entity\Auteur;
 use App\Repository\AuteurRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AuteurController extends AbstractController
 {
-    #[Route('/auteur', name: 'auteurs.create', methods: ['POST'])]
-    function creerauteur(Request $req, AuteurRepository $repository) {
-
+    #[Route("/auteur", name: "auteur.create", methods: ["POST"])]
+    function creerLivre(Request $req, AuteurRepository $repository)
+    {
+        // Récuperer les données depuis la requete
         $nom = $req->request->get('nom');
         $prenom = $req->request->get('prenom');
         $date = $req->request->get('date');
 
-        if (!isset($nom) || $nom == ""|| !isset($prenom) || $prenom == ""||!isset($date) || $date == "") {
-            return $this->json(['erreur' => "Remplir le champ !"]);
+        // Valider le titre
+        if (!isset($nom) || $nom == "" || !isset($prenom) || $prenom == "" || !isset($date) || $date == "") {
+            return $this->json(['erreur' => "Données obligatoires !"]);
         }
 
+        // Créer le livre
         $nouveauAuteur = new Auteur();
-        $nouveauAuteur->setNom($nom);
-        $nouveauAuteur->setPrenom($prenom);
-        $nouveauAuteur->setDate($date);
+        $nouveauAuteur->setNom($nom)->setPrenom($prenom)->setDate($date);
 
-        $auteurSave = $repository->save($nouveauAuteur,true);
+        // Enregistrer le livre dans la BDD
+        $auteurSavegarder = $repository->sauvegarder($nouveauAuteur, true);
 
-        return$this->json(['id' => $auteurSave->getId(), "nom" => $auteurSave->getNom(),"prenom" => $auteurSave->getPrenom(),"date" => $auteurSave->getDate()]);
-
+        return $this->json(['id' => $auteurSavegarder->getId(), "titre" => $auteurSavegarder->getNom()]);
     }
-    #[Route('/auteur', name: 'auteurs.tout', methods: ['GET'])]
-    // on injecte la dépendance dans les paramétres de la fonction pour cette fonction $repo est injecter dans LivreRepository
-    function recuperetTousAuteur(AuteurRepository $repo)
+
+    #[Route("/auteur", name: "auteur.all", methods: ["GET"])]
+    function recupererTousAuteurs(AuteurRepository $repo)
     {
         $auteurs = $repo->findAll();
-
-        $auteurTableau = [];
-
+        $auteursTab = [];
         foreach ($auteurs as $auteur) {
-            $auteurTableau[] = ['id' => $auteur->getId(), 'nom' => $auteur->getNom(),'prenom' => $auteur->getPrenom(),'date' => $auteur->getDate()];
+            $auteursTab[] = ['id' => $auteur->getId(), "nom" => $auteur->getNom()];
         }
 
-        return $this->json($auteurTableau);
+        return $this->json($auteursTab);
     }
-    #[Route('/auteur/{id}', name: 'auteurs.id', methods: ['GET'])]
-    function recupererAuteur(AuteurRepository $repo, $id)
-    {
-        // Récuperer le livre avec son Id
-        $auteur = $repo->find($id);
-        // Si l'auteur n'existe pas on retourne 404
-        if (!$auteur) {
-            return $this->json("l'auteur n'existe pas", Response::HTTP_NOT_FOUND);
-        }
 
-        // Retouner l'auteur récupérer
-        return $this->json(['id' => $auteur->getId(), 'nom' => $auteur->getNom(),'prenom' => $auteur->getPrenom(),'date' => $auteur->getDate()]);
+    #[Route("/auteur/{identifiant}", name: "auteur.one", methods: ["GET"])]
+    function recupererAuteur(AuteurRepository $repo, $identifiant)
+    {
+        $auteur = $repo->find($identifiant);
+        if (!$auteur) {
+            return $this->json('Auteur introuvable', Response::HTTP_NOT_FOUND);
+        }
+        return $this->json(['id' => $auteur->getId(), "nom" => $auteur->getNom()]);
     }
-    #[Route('/auteur/{id}', name: 'auteurs.update', methods: ['POST'])]
 
-    function mettreAjourAuteur($id, AuteurRepository $repo, Request $req)
+    #[Route("/auteur/{id}", name: "auteur.update", methods: ["POST"])]
+    function misAjourLivre(Request $req, AuteurRepository $repository, $id)
     {
-        // Récuperer le livre avec son Id
+        // Récuperer les données depuis la requete
+        $nom = $req->request->get('nom');
+        $prenom = $req->request->get('prenom');
+        $date = $req->request->get('date');
 
-        $auteur = $repo->find($id);
+        // Valider le titre
+        if (!isset($nom) || $nom == "" || !isset($prenom) || $prenom == "" || !isset($date) || $date == "") {
+            return $this->json(['erreur' => "Données obligatoires !"]);
+        }
 
-        // Si le livre n'existe pas on retourne 404
+        $auteur = $repository->find($id);
 
         if (!$auteur) {
-            return $this->json("l'auteur n'existe pas", Response::HTTP_NOT_FOUND);
+            return $this->json('Auteur introuvable', Response::HTTP_NOT_FOUND);
         }
-        // RECUPERER LE TITRE DU CORP DE LA REQUETE
-        $nouveauNom = $req->request->get('nom');
-        $nouveauPrenom = $req->request->get('prenom');
-        $nouveauDate = $req->request->get('date');
 
+        $auteur->setNom($nom)->setPrenom($prenom)->setDate($date);
 
-        // VALDER LE NOUVEAU TITRE
-        if (!isset($nom) || $nom == ""|| !isset($prenom) || $prenom == ""||!isset($date) || $date == "") {
-            return $this->json(['erreur' => "Remplir le champ !", Response::HTTP_BAD_REQUEST]);
-        }
-        // METTRE A JOUR LE LIVRE
-        $auteur->setNom($nouveauNom);
-        $auteur->setPrenom($nouveauPrenom);
-        $auteur->setDate($nouveauDate);
+        // Enregistrer le livre dans la BDD
+        $auteurSavegarder = $repository->sauvegarder($auteur, true);
 
-
-        // Enregistre le divre dans la BDD
-        $repo->save($auteur, true);
-
-        return $this->json($auteur->getNom() . 'auteur mis à jour !');
+        return $this->json(['id' => $auteurSavegarder->getId(), "titre" => $auteurSavegarder->getNom()]);
     }
-    #[Route('/livre/{id}', name: 'livres.supprimer', methods: ['DELETE'])]
 
-    function supprimerAuteur($id, AuteurRepository $repo)
+    #[Route("/auteur/{identifiant}", name: "auteur.one", methods: ["DELETE"])]
+    function supprimerAuteur(AuteurRepository $repo, $identifiant)
     {
-        // Récupérer le livre avec on identifiant
-        $auteur = $repo->find($id);
-
-        // Si le livre n'existe pas on retourne un 404
+        $auteur = $repo->find($identifiant);
         if (!$auteur) {
-            return $this->json("l'auteur' n'existe pas", Response::HTTP_NOT_FOUND);
+            return $this->json('Auteur introuvable', Response::HTTP_NOT_FOUND);
         }
-
-        // Supprmer le livre de la BDD
         $repo->supprimer($auteur);
-
-        // Retourner un message
         return $this->json("Auteur supprimer");
-
     }
 }
